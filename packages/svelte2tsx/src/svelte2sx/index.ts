04ea2d3f8e1d64381b4f754c182e947e6d4f5325
natklim <nataliaklimceva@gmail.com>
2025-05-24 21:08:57 +0500

    clean index.ts svelte2tsx

--- packages/svelte2tsx/src/svelte2sx/index.ts
+++ packages/svelte2tsx/src/svelte2sx/index.ts
@@ -1,4 +1,9 @@
-// Disable unused variable warnings during refactoring
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
- 
+// --- Civet Preprocessor (only if civet blocks are present) --- 
+let svelteContentForProcessing = svelte;
+let civetModuleInfo: any, civetInstanceInfo: any;
+if (svelte.includes('lang="civet"')) {
+    const _civet = preprocessCivet(svelte, svelteFilePath);
+    svelteContentForProcessing = _civet.code;
+    civetModuleInfo = _civet.module;
+    civetInstanceInfo = _civet.instance;
+} 
--- packages/svelte2tsx/src/svelte2tsx/index.ts
+++ packages/svelte2tsx/src/svelte2tsx/index.ts
@@ -1,7 +1,6 @@
 import MagicString from 'magic-string';
 import { convertHtmlxToJsx, TemplateProcessResult } from '../htmlxtojsx_v2';
 import { parseHtmlx } from '../utils/htmlxparser';
-import { chainSourceMaps, EncodedSourceMap } from '../utils/sourcemap-chaining';
 import { addComponentExport } from './addComponentExport';
 import { createRenderFunction } from './createRenderFunction';
 import { ExportedNames } from './nodes/ExportedNames';
@@ -12,15 +11,20 @@ import { createModuleAst, ModuleAst, processModuleScriptTag } from './processMod
 import path from 'path';
 import { parse, VERSION } from 'svelte/compiler';
 import { getTopLevelImports } from './utils/tsAst';
-import { preprocessCivet } from './utils/civetPreprocessor';
 
 function processSvelteTemplate(
     str: MagicString,
-    svelteParseFn: typeof import('svelte/compiler').parse,
-    options: { emitOnTemplateError?: boolean; namespace?: string; accessors?: boolean; mode?: 'ts' | 'dts'; typingsNamespace?: string; svelte5Plus: boolean; filename?: string; }
+    parse: typeof import('svelte/compiler').parse,
+    options: {
+        emitOnTemplateError?: boolean;
+        namespace?: string;
+        accessors?: boolean;
+        mode?: 'ts' | 'dts';
+        typingsNamespace?: string;
+        svelte5Plus: boolean;
+    }
 ): TemplateProcessResult {
-    const parseHtmlxOptions: { filename?: string; svelte5Plus: boolean } = { filename: options.filename, svelte5Plus: options.svelte5Plus };
-    const { htmlxAst, tags } = parseHtmlx(str.original, svelteParseFn, parseHtmlxOptions);
+    const { htmlxAst, tags } = parseHtmlx(str.original, parse, options);
     return convertHtmlxToJsx(str, htmlxAst, tags, options);
 }
 
@@ -41,155 +45,210 @@ export function svelte2tsx(
 ) {
     options.mode = options.mode || 'ts';
     options.version = options.version || VERSION;
-    const svelteFilePath = options.filename || 'unknown.svelte';
-    const svelteParseFn = options.parse || parse;
 
-    // --- Call Civet Preprocessor --- 
-    const { code: svelteContentForProcessing, module: civetModuleInfo, instance: civetInstanceInfo } = preprocessCivet(svelte, svelteFilePath);
-
-    const str = new MagicString(svelteContentForProcessing);
+    const str = new MagicString(svelte);
     const basename = path.basename(options.filename || '');
     const svelte5Plus = Number(options.version![0]) > 4;
     const isTsFile = options?.isTsFile;
 
+    // process the htmlx as a svelte template
     let {
-        htmlAst, moduleScriptTag, scriptTag, rootSnippets, slots, uses$$props,
-        uses$$slots, uses$$restProps, events, componentDocumentation, resolvedStores,
-        usesAccessors, isRunes
-    } = processSvelteTemplate(str, svelteParseFn, { 
-        emitOnTemplateError: options.emitOnTemplateError,
-        namespace: options.namespace,
-        accessors: options.accessors,
-        mode: options.mode,
-        typingsNamespace: options.typingsNamespace,
-        svelte5Plus,
-        filename: svelteFilePath
+        htmlAst,
+        moduleScriptTag,
+        scriptTag,
+        rootSnippets,
+        slots,
+        uses$$props,
+        uses$$slots,
+        uses$$restProps,
+        events,
+        componentDocumentation,
+        resolvedStores,
+        usesAccessors,
+        isRunes
+    } = processSvelteTemplate(str, options.parse || parse, {
+        ...options,
+        svelte5Plus
     });
 
+    /* Rearrange the script tags so that module is first, and instance second followed finally by the template
+     * This is a bit convoluted due to some trouble I had with magic string. A simple str.move(start,end,0) for each script wasn't enough
+     * since if the module script was already at 0, it wouldn't move (which is fine) but would mean the order would be swapped when the script tag tried to move to 0
+     * In this case we instead have to move it to moduleScriptTag.end. We track the location for the script move in the MoveInstanceScriptTarget var
+     */
     let instanceScriptTarget = 0;
+
     let moduleAst: ModuleAst | undefined;
 
     if (moduleScriptTag) {
         moduleAst = createModuleAst(str, moduleScriptTag);
-        if (moduleScriptTag.start !== 0) str.move(moduleScriptTag.start, moduleScriptTag.end, 0);
-        else instanceScriptTarget = moduleScriptTag.end;
+
+        if (moduleScriptTag.start != 0) {
+            //move our module tag to the top
+            str.move(moduleScriptTag.start, moduleScriptTag.end, 0);
+        } else {
+            //since our module script was already at position 0, we need to move our instance script tag to the end of it.
+            instanceScriptTarget = moduleScriptTag.end;
+        }
     }
 
-    const renderFunctionStart = scriptTag ? str.original.lastIndexOf('>', scriptTag.content.start) + 1 : instanceScriptTarget;
+    const renderFunctionStart = scriptTag
+        ? str.original.lastIndexOf('>', scriptTag.content.start) + 1
+        : instanceScriptTarget;
     const implicitStoreValues = new ImplicitStoreValues(resolvedStores, renderFunctionStart);
+    //move the instance script and process the content
     let exportedNames = new ExportedNames(str, 0, basename, isTsFile, svelte5Plus, isRunes);
     let generics = new Generics(str, 0, { attributes: [] } as any);
     let uses$$SlotsInterface = false;
-
     if (scriptTag) {
-        if (scriptTag.start !== instanceScriptTarget) str.move(scriptTag.start, scriptTag.end, instanceScriptTarget);
+        //ensure it is between the module script and the rest of the template (the variables need to be declared before the jsx template)
+        if (scriptTag.start != instanceScriptTarget) {
+            str.move(scriptTag.start, scriptTag.end, instanceScriptTarget);
+        }
         const res = processInstanceScriptContent(
-            str, scriptTag, events, implicitStoreValues, options.mode, 
-            moduleAst, isTsFile, basename, svelte5Plus, isRunes
+            str,
+            scriptTag,
+            events,
+            implicitStoreValues,
+            options.mode,
+            moduleAst,
+            isTsFile,
+            basename,
+            svelte5Plus,
+            isRunes
         );
         uses$$props = uses$$props || res.uses$$props;
         uses$$restProps = uses$$restProps || res.uses$$restProps;
         uses$$slots = uses$$slots || res.uses$$slots;
+
         ({ exportedNames, events, generics, uses$$SlotsInterface } = res);
     }
 
     exportedNames.usesAccessors = usesAccessors;
-    if (svelte5Plus) exportedNames.checkGlobalsForRunes(implicitStoreValues.getGlobals());
-
-    createRenderFunction({ str, scriptTag, scriptDestination: instanceScriptTarget, slots, events, exportedNames, uses$$props, uses$$restProps, uses$$slots, uses$$SlotsInterface, generics, svelte5Plus, isTsFile, mode: options.mode });
-
-    if (moduleScriptTag && moduleAst) { // Ensure moduleAst exists
-        processModuleScriptTag(str, moduleScriptTag, new ImplicitStoreValues(implicitStoreValues.getAccessedStores(), renderFunctionStart, scriptTag || options.mode === 'ts' ? undefined : (input: string) => `</>;${input}<>`), moduleAst);
-        if (!scriptTag) moduleAst.tsAst.forEachChild((node) => exportedNames.hoistableInterfaces.analyzeModuleScriptNode(node));
+    if (svelte5Plus) {
+        exportedNames.checkGlobalsForRunes(implicitStoreValues.getGlobals());
     }
 
-    if ((moduleScriptTag || scriptTag) && rootSnippets.length > 0) {
+    //wrap the script tag and template content in a function returning the slot and exports
+    createRenderFunction({
+        str,
+        scriptTag,
+        scriptDestination: instanceScriptTarget,
+        slots,
+        events,
+        exportedNames,
+        uses$$props,
+        uses$$restProps,
+        uses$$slots,
+        uses$$SlotsInterface,
+        generics,
+        svelte5Plus,
+        isTsFile,
+        mode: options.mode
+    });
+
+    // we need to process the module script after the instance script has moved otherwise we get warnings about moving edited items
+    if (moduleScriptTag) {
+        processModuleScriptTag(
+            str,
+            moduleScriptTag,
+            new ImplicitStoreValues(
+                implicitStoreValues.getAccessedStores(),
+                renderFunctionStart,
+                scriptTag || options.mode === 'ts' ? undefined : (input) => `</>;${input}<>`
+            ),
+            moduleAst
+        );
+        if (!scriptTag) {
+            moduleAst.tsAst.forEachChild((node) =>
+                exportedNames.hoistableInterfaces.analyzeModuleScriptNode(node)
+            );
+        }
+    }
+
+    if (moduleScriptTag && rootSnippets.length > 0) {
+        exportedNames.hoistableInterfaces.analyzeSnippets(rootSnippets);
+    }
+
+    if (moduleScriptTag || scriptTag) {
         let snippetHoistTargetForModule = 0;
-        if (scriptTag) snippetHoistTargetForModule = scriptTag.start + 1;
-        else if (moduleAst) {
+        if (rootSnippets.length) {
+            if (scriptTag) {
+                snippetHoistTargetForModule = scriptTag.start + 1; // +1 because imports are also moved at that position, and we want to move interfaces after imports
+            } else {
                 const imports = getTopLevelImports(moduleAst.tsAst);
                 const lastImport = imports[imports.length - 1];
-            snippetHoistTargetForModule = lastImport ? lastImport.end + moduleAst.astOffset : moduleAst.astOffset;
-            if (str.original.length > 0 || snippetHoistTargetForModule > 0) { // Avoid prepending to empty string at 0 if no content
+                snippetHoistTargetForModule = lastImport
+                    ? lastImport.end + moduleAst.astOffset
+                    : moduleAst.astOffset;
                 str.appendLeft(snippetHoistTargetForModule, '\n');
             }
         }
+
         for (const [start, end, globals] of rootSnippets) {
-            const hoist_to_module = moduleScriptTag && moduleAst && (globals.size === 0 || [...globals.keys()].every((id) => exportedNames.hoistableInterfaces.isAllowedReference(id)));
-            if (hoist_to_module) str.move(start, end, snippetHoistTargetForModule);
-            else if (scriptTag) str.move(start, end, renderFunctionStart);
+            const hoist_to_module =
+                moduleScriptTag &&
+                (globals.size === 0 ||
+                    [...globals.keys()].every((id) =>
+                        exportedNames.hoistableInterfaces.isAllowedReference(id)
+                    ));
+
+            if (hoist_to_module) {
+                str.move(start, end, snippetHoistTargetForModule);
+            } else if (scriptTag) {
+                str.move(start, end, renderFunctionStart);
+            }
         }
     }
 
-    addComponentExport({ str, canHaveAnyProp: !exportedNames.uses$$Props && (uses$$props || uses$$restProps), events, isTsFile, exportedNames, usesAccessors, usesSlots: slots.size > 0, fileName: svelteFilePath, componentDocumentation, mode: options.mode, generics, isSvelte5: svelte5Plus, noSvelteComponentTyped: options.noSvelteComponentTyped });
+    addComponentExport({
+        str,
+        canHaveAnyProp: !exportedNames.uses$$Props && (uses$$props || uses$$restProps),
+        events,
+        isTsFile,
+        exportedNames,
+        usesAccessors,
+        usesSlots: slots.size > 0,
+        fileName: options?.filename,
+        componentDocumentation,
+        mode: options.mode,
+        generics,
+        isSvelte5: svelte5Plus,
+        noSvelteComponentTyped: options.noSvelteComponentTyped
+    });
 
     if (options.mode === 'dts') {
-        if (options.noSvelteComponentTyped) str.prepend('import { SvelteComponent } from "svelte"\n\n');
-        else str.prepend('import { SvelteComponentTyped } from "svelte"\n\n');
+        // Prepend the import which is used for TS files
+        // The other shims need to be provided by the user ambient-style,
+        // for example through filenames.push(require.resolve('svelte2tsx/svelte-shims.d.ts'))
+        // TODO replace with SvelteComponent for Svelte 5, keep old for backwards compatibility with Svelte 3
+        if (options.noSvelteComponentTyped) {
+            str.prepend('import { SvelteComponent } from "svelte"\n' + '\n');
+        } else {
+            str.prepend('import { SvelteComponentTyped } from "svelte"\n' + '\n');
+        }
         let code = str.toString();
-        code = code.replace(/<\/><\/>;/g, '').replace(/<>.*<\/>/s, '').replace('\n() => ();', ''); // Adjusted regex for empty tags
-        return { code };
+        // Remove all tsx occurences and the template part from the output
+        code = code
+            // prepended before each script block
+            .replace('<></>;', '')
+            .replace('<></>;', '')
+            // tsx in render function
+            .replace(/<>.*<\/>/s, '')
+            .replace('\n() => ();', '');
+
+        return {
+            code
+        };
     } else {
         str.prepend('///<reference types="svelte" />\n');
-        const finalMapFromMagicString = str.generateMap({
-            hires: true,
-            file: svelteFilePath,
-            source: svelteFilePath,
-            includeContent: true
-        });
-        
-        let mapToReturn: EncodedSourceMap = {
-            version: finalMapFromMagicString.version,
-            sources: finalMapFromMagicString.sources,
-            names: finalMapFromMagicString.names,
-            mappings: finalMapFromMagicString.mappings,
-            file: finalMapFromMagicString.file || undefined,
-            sourcesContent: finalMapFromMagicString.sourcesContent ? finalMapFromMagicString.sourcesContent.map(sc => sc === null ? undefined : sc) : undefined
+        return {
+            code: str.toString(),
+            map: str.generateMap({ hires: true, source: options?.filename }),
+            exportedNames: exportedNames.getExportsMap(),
+            events: events.createAPI(),
+            // not part of the public API so people don't start using it
+            htmlAst
         };
-
-        const generatedCodeWithMarkers = str.toString();
-        let generatedCodeClean = generatedCodeWithMarkers;
-        const finalCodeToReturn = generatedCodeClean;
-
-        if (civetModuleInfo && moduleScriptTag) {
-            try {
-                const chainedModuleMap = chainSourceMaps(
-                    mapToReturn,
-                    civetModuleInfo.map,
-                    civetModuleInfo.tsStartInSvelteWithTs,
-                    civetModuleInfo.tsEndInSvelteWithTs,
-                    moduleScriptTag.content.start,
-                    svelteContentForProcessing,
-                    finalCodeToReturn,
-                    civetModuleInfo.originalContentStartLine
-                );
-                mapToReturn = chainedModuleMap;
-            } catch (e: any) { console.error('Module Civet sourcemap chaining error (New Path):', e.message, e.stack); }
-        }
-
-        if (civetInstanceInfo && scriptTag) {
-            try {
-                const chainedInstanceMap = chainSourceMaps(
-                    mapToReturn,
-                    civetInstanceInfo.map,
-                    civetInstanceInfo.tsStartInSvelteWithTs,
-                    civetInstanceInfo.tsEndInSvelteWithTs,
-                    scriptTag.content.start,
-                    svelteContentForProcessing,
-                    finalCodeToReturn,
-                    civetInstanceInfo.originalContentStartLine
-                );
-                mapToReturn = chainedInstanceMap;
-            } catch (e: any) { console.error('Instance Civet sourcemap chaining error (New Path):', e.message, e.stack); }
-        }
-
-        if (mapToReturn.sources && mapToReturn.sourcesContent) {
-            const svelteFileIndex = mapToReturn.sources.indexOf(svelteFilePath);
-            if (svelteFileIndex !== -1) {
-                mapToReturn.sourcesContent[svelteFileIndex] = svelte;
-            }
-        }
-
-        return { code: finalCodeToReturn, map: mapToReturn, exportedNames: exportedNames.getExportsMap(), events: events.createAPI(), htmlAst };
     }
 }
